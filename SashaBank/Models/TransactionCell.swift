fdd8c2495e6a4288170d6e06b532cd15795b8af6
Alex Dearden <alex.dearden@matchesfashion.com>
2017-10-27 12:17:27 +0100

    Cleanup unused method.

--- SashaBank/Models/TransactionCell.swift
+++ SashaBank/Models/TransactionCell.swift
@@ -13,11 +13,5 @@ class TransactionCell: UITableViewCell {
     @IBOutlet weak var transactionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var amountLabel: UILabel!
-        
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
